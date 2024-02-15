0c30d67c915c84da0a9906577b3b50de75ef69e2
s-heppner <s.heppner@iat.rwth-aachen.de>
2024-02-15 14:41:36 +0100

    Add a first prototype of the generation script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # aas-core-schemagen
-Helper scripts to generate schemas using aas-core-codegen
+Helper scripts to generate AAS schemas using the tools from [aas-core-works](https://github.com/aas-core-works/).
+
+## DISCLAIMER
+This is meant as an expert helper tool for generating the schemas 
+in the development phase for a publication
+by the Industrial Digital Twin Association (IDTA). 
+The resulting schemas are not tested, nor in any way official.
+
+**Use at your own risk.**
+
+## Requirements: 
+
+- Linux 
+- Python >= 3.9
+
+## Usage: 
+
+1. Adapt the `.env` file with the current versions of aas-core-meta and aas-core-codegen
+2. Run `bash generate_schemas.sh`
---
+++ generate_schemas.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Read in variables
+source .env
+echo "Generate schemas using"
+echo "    aas-core-meta@    ${AAS_CORE_META_COMMIT}"
+echo "    aas-core-codegen@ ${AAS_CORE_CODEGEN_COMMIT}"
+echo ""
+
+# Generate a new Python virtual environment
+python3 -m venv venv-codegen
+source venv-codegen/bin/activate
+which python3
+
+# Pull repositories
+mkdir -p ./temp
+mkdir -p "${OUTPUT_DIR}"
+cd ./temp || return 1
+git clone https://github.com/aas-core-works/aas-core-meta.git
+cd ./aas-core-meta || return 1
+git checkout "${AAS_CORE_META_COMMIT}"
+
+# Install dependencies
+pip install -e .[dev]
+pip install git+https://github.com/aas-core-works/aas-core-codegen@"${AAS_CORE_CODEGEN_COMMIT}"
+
+# Generate schemas
+echo "Generate JSON Schema"
+aas-core-codegen \
+  --model_path "${AAS_CORE_META_MODEL_FILE}" \
+  --snippets_dir "../../${SNIPPETS_DIR}"/jsonschema \
+  --output_dir "../../${OUTPUT_DIR}" \
+  --target jsonschema
+
+echo "Generate XSD"
+aas-core-codegen \
+  --model_path "${AAS_CORE_META_MODEL_FILE}" \
+  --snippets_dir "../../${SNIPPETS_DIR}"/xsd \
+  --output_dir "../../${OUTPUT_DIR}" \
+  --target xsd
+
+echo "Generate RDFS"
+aas-core-codegen \
+  --model_path "${AAS_CORE_META_MODEL_FILE}" \
+  --snippets_dir "../../${SNIPPETS_DIR}"/rdf \
+  --output_dir "../../${OUTPUT_DIR}" \
+  --target rdf_shacl
+
+# Clean up
+cd ../..
+rm -rf ./temp
+rm -r ./venv-codegen
